220cb6b78ddfc849c18a495e0306ac46ae52dd5f
Both Valerian <valeriangboth97@gmail.com>
2023-10-12 20:58:35 +0300

    Edit index.html

---
+++ app.mjs
@@ -0,0 +1,9 @@
+let buttons = document.querySelectorAll('.join-us-button');
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', redirectToSecondPage);
+});
+
+function redirectToSecondPage() {
+  window.location.href = 'finalpg.html';
+}
